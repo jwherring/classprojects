8196458e23ce996149a1ac4c29f46f8956782be4
Joshua Herring <jwherring@gmx.com>
2013-03-09 13:23:07 -0500

    ensure that projects are not destroyed if they have participants; associated projects with classes in the model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,16 @@
 class Project < ActiveRecord::Base
+  has_many :project_participants 
+  belongs_to :course
+  before_destroy :ensure_not_referenced_by_any_project_participant
+
+  private
+
+  def ensure_not_referenced_by_any_project_participant
+    if project_participants.empty?
+      return true
+    else
+      errors.add(:base, "Project has participants")
+    end
+  end
+
 end
