d03d1f9acde60eb666606d81e9988e6a05edab4e
Joshua Herring <jwherring@gmx.com>
2013-03-10 12:18:22 -0400

    use new syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :project_participants, dependent: :destroy
   has_many :user_roles, dependent: :destroy
-  has_many :roles, :through => :user_roles
+  has_many :roles, through: :user_roles
   has_many :evaulations, dependent: :destroy
   accepts_nested_attributes_for :roles
 end
