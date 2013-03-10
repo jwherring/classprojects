fe6398e85da060af39da9c24a978f5106750fa9c
Joshua Herring <jwherring@gmx.com>
2013-03-10 11:46:01 -0400

    fix user_roles migration, add new schema file to version control

--- db/migrate/20130310153848_create_user_roles.rb
+++ db/migrate/20130310153848_create_user_roles.rb
@@ -1,8 +1,8 @@
 class CreateUserRoles < ActiveRecord::Migration
   def change
     create_table :user_roles do |t|
-      t.reference :user
-      t.reference :role
+      t.references :user
+      t.references :role
 
       t.timestamps
     end
