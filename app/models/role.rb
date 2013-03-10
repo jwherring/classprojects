500c8bc8661a8d057c4f78ee133493186513de2e
Joshua Herring <jwherring@gmx.com>
2013-03-10 11:58:49 -0400

    fix user roles references

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,16 @@
 class Role < ActiveRecord::Base
-  has_many :users
+  has_many :user_roles
+  before_destroy :ensure_not_referenced_by_any_user
 end
+
+private
+
+def ensure_not_referenced_by_any_user
+  if user_roles.empty?
+    return True
+  else
+    errors.add(:base, "Role has users")
+  end
+
+end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :project_participants, dependent: :destroy
-  has_many :roles, dependent: :destroy
+  has_many :user_roles, dependent: :destroy
   has_many :evaulations, dependent: :destroy
   accepts_nested_attributes_for :roles
 end
