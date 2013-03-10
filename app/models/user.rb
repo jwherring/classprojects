8c7aa18ac65e62a8e9edf1af60cd213c1362b454
Joshua Herring <jwherring@gmx.com>
2013-03-10 11:55:06 -0400

    Roles now have many users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_many :project_participants, dependent: :destroy
   has_many :roles, dependent: :destroy
   has_many :evaulations, dependent: :destroy
+  accepts_nested_attributes_for :roles
 end
