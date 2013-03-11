3c0c71338f6832997e6de1135941b4e909d63432
Joshua Herring <jwherring@gmx.com>
2013-03-11 19:06:08 -0400

    fix another class_id typo so we can change which courses projects belong to

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -69,6 +69,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:title, :abstract, :website, :class_id)
+      params.require(:project).permit(:title, :abstract, :website, :course_id)
     end
 end
