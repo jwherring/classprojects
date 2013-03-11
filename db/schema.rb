3698d22d8d67b2dae3ee0a14bed245f842396738
Joshua Herring <jwherring@gmx.com>
2013-03-11 04:18:52 -0400

    add password digest field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130310153848) do
+ActiveRecord::Schema.define(version: 20130311081759) do
 
   create_table "courses", force: true do |t|
     t.string   "title"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 20130310153848) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
