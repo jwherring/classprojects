a6eda9a24ef1a875549c568885155c7f32baf0c5
Joshua Herring <jwherring@gmx.com>
2013-03-11 19:17:36 -0400

    validate numericality of input to rating on evaluation

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,4 +2,5 @@ class Evaluation < ActiveRecord::Base
   belongs_to :project
   belongs_to :user
   validates_length_of :comment, :maximum => 300
+  validates_numericality_of :rating, :minimum => 1, :maximum => 9
 end
