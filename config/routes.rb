4fc094f9f1fdb01ffb770c48675dddd9e109f31e
Joshua Herring <jwherring@gmx.com>
2013-03-11 19:30:27 -0400

    custom routes for login, logout, signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Classprojects::Application.routes.draw do
   get "landing/index"
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
 
   root to: 'landing#index'
 
