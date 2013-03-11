2c6296b4201e6a339cdb28f65f92591e074321e7
Joshua Herring <jwherring@gmx.com>
2013-03-11 14:12:00 -0400

    authentication logic in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Welcome, #{user.first_name}!"
+    else
+      flash.now.alert = "Email or password is invalid"
+      render "new" 
+    end
+
+  end
 end
