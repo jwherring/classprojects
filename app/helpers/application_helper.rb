ea7aaa063822067d32824a52832203891c57d2d6
Joshua Herring <jwherring@gmx.com>
2013-03-13 04:46:36 -0400

    make my projects page sortable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,10 @@ module ApplicationHelper
   def sortable(column, title=nil)
     title ||= column.titleize
     sort_order = (column == params[:sort] && params[:sort_order] == 'asc') ? 'desc' : 'asc'
-    link_to title, {:sort => column, :sort_order => sort_order}, :class => "sortheader"
+    if params[:limit]
+      link_to title, {:sort => column, :sort_order => sort_order, :limit => :true}, :class => "sortheader"
+    else 
+      link_to title, {:sort => column, :sort_order => sort_order}, :class => "sortheader"
+    end
   end
 end
